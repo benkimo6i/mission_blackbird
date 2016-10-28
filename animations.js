8c513b113b3d1c538de83faf8808a1ee16104029
Benjamin Kim <benjaminkim14@gmail.com>
2016-10-27 21:21:08 -0400

    we are getting closer to going live

--- animations.js
+++ animations.js
@@ -16,13 +16,42 @@ $( document ).ready(function() {
   		opacity: 1
   	}, 1500);
 
-  	$('.social-icons').on('mouseover', function (e) {
+  	// $('.social-icons').on('mouseover', function (e) {
 
-  		$('.twitter').addClass('animated zoomIn')
-  		$('.instagram').addClass('animated zoomIn')
-  		$('.linkedin').addClass('animated zoomIn')
-  	});
+  	// 	$('.twitter').addClass('animated zoomIn')
+  	// 	$('.instagram').addClass('animated zoomIn')
+  	// 	$('.linkedin').addClass('animated zoomIn')
+  	// });
+
+    // social media animations
+
+    var twitter = $('.twitter');
+    var instagram = $('.instagram');
+    var linkedin = $('.linkedin');
+    var logo = $('.logoImg')
+
+
+    function animateSocialIcons(icon){
+      icon.on('mouseover', function (e) {
+        $(this).addClass('animated zoomIn')
+      });
+    };
+
+    animateSocialIcons(twitter);
+    animateSocialIcons(instagram);
+    animateSocialIcons(linkedin);
+
 
 
  
 });
+
+
+
+
+
+
+
+
+
+
